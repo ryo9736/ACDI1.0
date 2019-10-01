14a68b429ba334659e376fefbd792d9c8ded4a08
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-01 18:21:32 +0900

    add:"gem 'rails_admin'実装 "

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'tops#index'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
