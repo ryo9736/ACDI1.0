469bfe2887d914ea2cd1b1f47281061b6e77a919
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-01 17:26:40 +0900

    add:"gem'device'導入"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin_users
-  devise_for :users
   root 'tops#index'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
