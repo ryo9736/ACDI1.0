a82bf8c4bdaf83ac62f0d92d30535d4980c139db
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-02 16:34:05 +0900

    add:"userテーブルにuser情報を追加"

---
+++ db/migrate/20191002064757_add_infomation_to_users.rb
@@ -0,0 +1,10 @@
+class AddInfomationToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :histroy, :integer
+    add_column :users, :area, :string
+    add_column :users, :gym, :string, default:"自宅"
+    add_column :users, :goal, :string
+    add_column :users, :image, :text
+  end
+end
