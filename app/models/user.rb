7040bbb9e38528834a6c264250664e916d0dd279
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-07 14:32:00 +0900

    add:"messageモデルとentryモデルのアソシエーション定義"

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :posts
   has_many :favorites, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 end
