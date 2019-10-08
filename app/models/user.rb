18f1fac7899a0325c2204248840c0ef167550a01
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-08 11:59:44 +0900

    add:"reviewモデルのアソシエーション定義"

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
