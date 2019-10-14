89ce208e0e62b64dacd9fa1a14cf9240260a0beb
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-14 17:49:55 +0900

    fix:"ログイン後のルート設定"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
       end
   
     def after_sign_in_path_for(resource)
-      root_path
+      posts_path
     end
 
      def after_sign_up_path_for(resource)
