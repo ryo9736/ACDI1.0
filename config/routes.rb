a36b9280eb592d384c561d02a4708304249cce13
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-14 17:49:10 +0900

    add:"homeコントローラー作成"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'tops#index'
+  root 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
