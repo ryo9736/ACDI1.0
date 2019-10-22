c95de3c1240c6c3f07a265c9dc63f2aee3d04479
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-22 13:27:21 +0900

    add:"userモデルにバリデーション追加"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true,
             length: { maximum: 30 },
             uniqueness: true,
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :trainer,
-  validates :trainee,
-  validates :image, presence: true
   mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
