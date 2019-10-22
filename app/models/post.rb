05b133a1f4ec67d2d9ea1022e03f8f29c712e2e8
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-22 18:18:53 +0900

    add:"postモデルにバリデーション実装"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
+  validates :title,presence: true,
+             length: { in: 1..50 }
+  validates :purpose,presence: true,
+             length: { in: 1..300 }
+  validates :current_status,presence: true,
+             length: { in: 1..300 }
+  validates :target,presence: true,
+             length: { in: 1..300 }
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
