425c6db61eb0c3e1b3190cff1dcec14ac6ce0c10
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-23 15:15:59 +0900

    add:"post機能のテスト作成"

---
+++ spec/features/post.spec.rb
@@ -0,0 +1,110 @@
+require 'rails_helper'
+
+RSpec.feature "投稿機能", type: :feature do
+
+  background do
+    FactoryBot.create(:user)
+    FactoryBot.create(:second_user)
+  end
+
+  before do
+    visit new_user_session_path
+
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: '123456'
+    click_on 'ログインする'
+    click_on '新規投稿'
+    fill_in 'post_title', with:"テストを書く"
+    fill_in 'post_purpose', with:"テストを書き終わること"
+    fill_in 'post_current_status', with:"テストを書いている"
+    fill_in 'post_target', with: "'テストを書く"
+    fill_in 'post_tag_list', with:"上半身"
+    click_on '登録する'
+  end
+
+  scenario "新規投稿機能のテスト" do
+    click_on '新規投稿'
+    fill_in 'post_title', with:"テストを書く"
+    fill_in 'post_purpose', with:"テストを書き終わること"
+    fill_in 'post_current_status', with:"テストを書いている"
+    fill_in 'post_target', with: "'テストを書く"
+    fill_in 'post_tag_list', with:"上半身"
+    click_on '登録する'
+    expect(page).to have_text "投稿一覧"
+  end
+
+  scenario "投稿一覧機能のテスト" do
+    click_on '新規投稿'
+    fill_in 'post_title', with:"テストを書く"
+    fill_in 'post_purpose', with:"テストを書き終わること"
+    fill_in 'post_current_status', with:"テストを書いている"
+    fill_in 'post_target', with: "'テストを書く"
+    fill_in 'post_tag_list', with:"上半身"
+    click_on '登録する'
+    click_on '投稿一覧'
+    expect(page).to have_text "トレーナー"
+  end
+
+  scenario "投稿編集機能のテスト" do
+    click_on '新規投稿'
+    fill_in 'post_title', with:"テストを書く"
+    fill_in 'post_purpose', with:"テストを書き終わること"
+    fill_in 'post_current_status', with:"テストを書いている"
+    fill_in 'post_target', with: "'テストを書く"
+    click_on '登録する'
+    click_on '編集'
+    fill_in  'post_start_reason', with:"テストを書く必要があるから"
+    click_on '更新する'
+    expect(page).to have_text "投稿情報"
+  end
+
+  scenario "投稿削除機能のテスト" do
+    click_on '新規投稿'
+    fill_in 'post_title', with:"テストを書く"
+    fill_in 'post_purpose', with:"テストを書き終わること"
+    fill_in 'post_current_status', with:"テストを書いている"
+    fill_in 'post_target', with: "'テストを書く"
+    click_on '登録する'
+    click_on '削除'
+    expect(page).to have_text "トレーナー"
+  end
+
+  scenario "投稿お気に入り機能のテスト" do
+    click_on 'ログアウト'
+    fill_in 'user_email', with: 'second@second.com'
+    fill_in 'user_password', with: '678910'
+    click_on 'ログインする'
+    click_on '投稿詳細'
+    click_on 'お気に入りする'
+    click_on '投稿詳細'
+    expect(page).to have_text "お気に入り解除"
+  end
+
+
+  scenario "投稿タグ検索機能のテスト" do
+    click_on 'ログアウト'
+    fill_in 'user_email', with: 'second@second.com'
+    fill_in 'user_password', with: '678910'
+    click_on 'ログインする'
+    click_on '新規投稿'
+    fill_in 'post_title', with:"テストを書く"
+    fill_in 'post_purpose', with:"テストを書き終わること"
+    fill_in 'post_current_status', with:"テストを書いている"
+    fill_in 'post_target', with: "'テストを書く"
+    fill_in 'post_tag_list', with:"下半身"
+    click_on '登録する'
+
+    visit posts_path
+    click_link '下半身'
+    expect(page).to have_no_text "上半身"
+  end
+
+
+
+
+
+
+end
+
+#bin/rspec spec/features/post.spec.rb
+#save_and_open_page
