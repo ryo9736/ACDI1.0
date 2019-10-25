d7059d4a27777f3d569635f279273c0c42d35f5c
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-25 19:11:46 +0900

    add:"seedデータ作成"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+    email = Faker::Internet.email
+    password = "password"
+    User.create!(email: email,
+                 password: password,
+                 password_confirmation: password,
+                 )
+  end
