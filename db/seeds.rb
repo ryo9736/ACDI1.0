55f469d553a95b964e7181a4141c9e8985a141d2
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-25 21:01:58 +0900

    fix:"エラー修正"

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ User.create!(name: "test_user",
 
                20.times do |n|
                 name = Faker::Games::Pokemon.name
-                email = Faker::Internet.email.characters(10)
+                email = Faker::Internet.email
                 password = "password"
                 User.create!(name: name,
                              email: email,
