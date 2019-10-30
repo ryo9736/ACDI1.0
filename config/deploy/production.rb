f7da91fc6e7cb58389f207b997d917829dc2a889
ryotaro yamashita <domdomgangan@gmail.com>
2019-10-30 17:38:31 +0900

    fix:"デプロイエラー修正"

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.115.187.185', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/yamashita-ryotaro/.ssh/id_rsa'
+set :ssh_options, keys: '/Users/yamashitaryoutarou/.ssh/id_rsa'
