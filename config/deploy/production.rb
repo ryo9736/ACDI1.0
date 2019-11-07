b6799af2756673eb1733aeab0d37bb7d44eff02b
ryotaro yamashita <domdomgangan@gmail.com>
2019-11-07 17:18:35 +0900

    fix:"AWSエラー修正"

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,7 @@
 server '3.115.187.185', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/yamashitaryoutarou/.ssh/id_rsa'
+
+set :ssh_options, {
+  port: 22,
+  forward_agent: true,
+  keys: ['/Users/yamashitaryoutarou/.ssh/id_rsa']
+}
